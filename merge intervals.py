9b2807ad59dd4b3a18f25b497e2917c8dab2601c
AyushRaj-001 <ansh23pathak@gmail.com>
2026-02-14 19:12:51 +0530

    merge intervals.py

---
+++ merge intervals.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+    
+        if len(intervals) <= 1:
+            return intervals
+        
+        
+        intervals.sort(key=lambda x: x[0])
+        
+        merged = []
+        merged.append(intervals[0])
+        
+        for i in range(1, len(intervals)):
+            start, end = intervals[i]
+            last_end = merged[-1][1]
+            
+            # If overlapping
+            if start <= last_end:
+                merged[-1][1] = max(last_end, end)
+            else:
+                merged.append([start, end])
+        
+        return merged
