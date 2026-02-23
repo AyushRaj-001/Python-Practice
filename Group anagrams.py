f25a9c11a083de796e3ca6b1b61d3f7b645dd5d7
AyushRaj-001 <ansh23pathak@gmail.com>
2026-02-23 18:57:43 +0530

    Group anagrams.py

---
+++ Group anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        from collections import defaultdict
+        
+        hashmap = defaultdict(list)
+        
+        for word in strs:
+            key = "".join(sorted(word))
+            hashmap[key].append(word)
+        
+        return list(hashmap.values())
